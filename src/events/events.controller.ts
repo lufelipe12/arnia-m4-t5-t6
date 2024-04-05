5d52c0401bd83e67bc8bd91b48c407879c5f0ca1
Vitor Reis <reis_vitor@outlook.com>
2024-04-04 22:09:20 -0300

    feat: class 03/04

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -7,6 +7,9 @@ import {
   Param,
   Delete,
   UseGuards,
+  UploadedFile,
+  UseInterceptors,
+  Res,
 } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { EventsService } from './events.service';
@@ -14,6 +17,9 @@ import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { CurrentUser } from 'src/decorators/current-user.decorator';
 import { CurrentUserDto } from 'src/decorators/dto/current-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { Response } from 'express';
 
 @Controller('events')
 export class EventsController {
@@ -33,6 +39,40 @@ export class EventsController {
   @Post(':id/participate')
   partipate(@Param('id') id: string, @CurrentUser() user: CurrentUserDto) {
     return this.eventsService.partipate(+id, user.sub);
+  }
+
+  @Post(':id/upload-photo')
+  @UseInterceptors(
+    FileInterceptor('photo', {
+      limits: {
+        fileSize: 1024 * 1024, // tamanho maximo
+      },
+      storage: diskStorage({
+        destination: './uploads', // onde salvar
+        filename: (req, file, cb) => {
+          const filename = `${Date.now()}-${file.originalname}`;
+          cb(null, filename);
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+          // validação de tipo
+          return cb(null, false);
+        }
+        return cb(null, true);
+      },
+    }),
+  )
+  uploadPhoto(
+    @UploadedFile()
+    file: Express.Multer.File,
+  ) {
+    console.log('teste', file);
+  }
+
+  @Get('/photo/:filename')
+  getPhoto(@Param('filename') filename: string, @Res() res: Response) {
+    return res.sendFile(filename, { root: './uploads' });
   }
 
   @Get(':id')
